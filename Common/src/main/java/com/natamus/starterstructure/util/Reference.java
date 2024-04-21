2f6ffd5d52672cca9c28970957efb5716d0d669b
Rick South <rick@natamus.com>
2024-04-21 22:33:08 +0200

    (v3.6) Fixed the structure offset position not being parsed correctly.

--- Common/src/main/java/com/natamus/starterstructure/util/Reference.java
+++ Common/src/main/java/com/natamus/starterstructure/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.starterstructure.util;
 public class Reference {
 	public static final String MOD_ID = "starterstructure";
 	public static final String NAME = "Starter Structure";
-	public static final String VERSION = "3.5";
+	public static final String VERSION = "3.6";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
