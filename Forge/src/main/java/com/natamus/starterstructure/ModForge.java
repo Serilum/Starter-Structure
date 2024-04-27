774c1aa6926602f809f2e192ffe68762de4ff2b4
Rick South <rick@natamus.com>
2024-04-28 01:23:39 +0200

    Starter Structure source code for 1.20.5.

---
+++ Forge/src/main/java/com/natamus/starterstructure/ModForge.java
@@ -0,0 +1,40 @@
+package com.natamus.starterstructure;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.starterstructure.forge.config.IntegrateForgeConfig;
+import com.natamus.starterstructure.forge.events.ForgeStructureCreationEvents;
+import com.natamus.starterstructure.forge.events.ForgeStructureProtectionEvents;
+import com.natamus.starterstructure.forge.events.ForgeStructureSpawnPointEvents;
+import com.natamus.starterstructure.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ForgeStructureProtectionEvents());
+    	MinecraftForge.EVENT_BUS.register(new ForgeStructureCreationEvents());
+        MinecraftForge.EVENT_BUS.register(new ForgeStructureSpawnPointEvents());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
