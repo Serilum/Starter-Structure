becbeae75a36a2b057eff7af5c55d34da54cc8e0
Rick South <rick@natamus.com>
2024-10-24 00:49:01 +0200

    Starter Structure source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/starterstructure/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.starterstructure.util;
+
+public class Reference {
+	public static final String MOD_ID = "starterstructure";
+	public static final String NAME = "Starter Structure";
+	public static final String VERSION = "3.9";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
---
+++ NeoForge/src/main/java/com/natamus/starterstructure/neoforge/events/NeoForgeStructureProtectionEvents.java
@@ -0,0 +1,70 @@
+package com.natamus.starterstructure.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.starterstructure.events.StructureProtectionEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.ExplosionEvent;
+import net.neoforged.neoforge.event.level.PistonEvent;
+
+@EventBusSubscriber
+public class NeoForgeStructureProtectionEvents {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (!StructureProtectionEvents.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null)) {
+			e.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onBlockPlace(BlockEvent.EntityPlaceEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		Entity entity = e.getEntity();
+		if (!(entity instanceof LivingEntity)) {
+			return;
+		}
+
+		if (!StructureProtectionEvents.onBlockPlace(level, e.getPos(), e.getPlacedBlock(), (LivingEntity)entity, null)) {
+			e.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPistonMove(PistonEvent.Pre e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (!StructureProtectionEvents.onPistonMove(level, e.getPos(), e.getDirection(), e.getPistonMoveType().isExtend)) {
+			e.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onTNTExplode(ExplosionEvent.Detonate e) {
+		StructureProtectionEvents.onTNTExplode(e.getLevel(), null, e.getExplosion());
+	}
+
+	@SubscribeEvent
+	public static void onLivingAttack(LivingIncomingDamageEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (!StructureProtectionEvents.onLivingAttack(livingEntity.level(), livingEntity, e.getSource(), e.getAmount())) {
+			e.setCanceled(true);
+		}
+	}
+}
