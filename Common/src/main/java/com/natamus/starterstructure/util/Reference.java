6e56e5dfc59227c2354a39223a9c00d48ba303d7
Rick South <rick@natamus.com>
2026-03-26 01:25:58 +0100

    Starter Structure source code for 26.1.0.

---
+++ Common/src/main/java/com/natamus/starterstructure/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.starterstructure.util;
+
+public class Reference {
+	public static final String MOD_ID = "starterstructure";
+	public static final String NAME = "Starter Structure";
+	public static final String VERSION = "4.5";
+	public static final String ACCEPTED_VERSIONS = "[26.1.0]";
+}
---
+++ Forge/src/main/java/com/natamus/starterstructure/forge/events/ForgeStructureSpawnPointEvents.java
@@ -0,0 +1,34 @@
+package com.natamus.starterstructure.forge.events;
+
+import com.natamus.starterstructure.events.StructureSpawnPointEvents;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeStructureSpawnPointEvents {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeStructureSpawnPointEvents.class);
+	}
+
+	@SubscribeEvent
+	public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent e) {
+		Player player = e.getEntity();
+		Level world = player.level();
+		if (world.isClientSide()) {
+			return;
+		}
+
+		StructureSpawnPointEvents.onPlayerRespawn(null, (ServerPlayer)player, true);
+	}
+
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		StructureSpawnPointEvents.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+}
