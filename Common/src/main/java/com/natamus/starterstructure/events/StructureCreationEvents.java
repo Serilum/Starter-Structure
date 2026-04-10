8a51c52e26b339494ccabd6bab90485ca31ea108
Rick South <rick@natamus.com>
2026-04-10 02:42:47 +0200

    Starter Structure source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/starterstructure/events/StructureCreationEvents.java
@@ -0,0 +1,55 @@
+package com.natamus.starterstructure.events;
+
+import com.natamus.collective.functions.FeatureFunctions;
+import com.natamus.collective.functions.TaskFunctions;
+import com.natamus.starterstructure.config.ConfigHandler;
+import com.natamus.starterstructure.util.Util;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.levelgen.WorldOptions;
+import net.minecraft.world.level.storage.LevelData.RespawnData;
+import net.minecraft.world.level.storage.ServerLevelData;
+
+public class StructureCreationEvents {
+	public static InteractionResult onLevelSpawn(ServerLevel serverLevel, ServerLevelData serverLevelData) {
+		TaskFunctions.enqueueCollectiveServerTask(serverLevel.getServer(), () -> {
+			if (ConfigHandler.shouldGenerateStructure) {
+				BlockPos spawnPos = null;
+
+				BlockPos structurePos = Util.generateSchematic(serverLevel);
+				if (structurePos != null) {
+					spawnPos = structurePos.immutable();
+					if (ConfigHandler.shouldUseSpawnCoordOffsets) {
+						spawnPos = spawnPos.offset(ConfigHandler.spawnXCoordOffset, ConfigHandler.spawnYCoordOffset, ConfigHandler.spawnZCoordOffset).immutable();
+					}
+
+					RespawnData oldRespawnData = serverLevel.getRespawnData();
+					RespawnData newRespawnData = RespawnData.of(oldRespawnData.dimension(), spawnPos, oldRespawnData.yaw(), oldRespawnData.pitch());
+
+					serverLevel.setRespawnData(newRespawnData);
+				}
+
+				if (spawnPos != null) {
+					try {
+						WorldOptions worldOptions = serverLevel.getServer().getWorldGenSettings().options();
+
+						if (worldOptions.generateBonusChest()) {
+							FeatureFunctions.placeBonusChest(serverLevel, spawnPos);
+						}
+					}
+					catch (NoSuchMethodError ignored) {}
+				}
+			}
+		}, 1);
+
+		if (ConfigHandler.shouldUseSpawnCoordinates || ConfigHandler.shouldUseSpawnCoordOffsets || ConfigHandler.shouldUseStructureOffset) {
+			return InteractionResult.SUCCESS;
+		}
+		return InteractionResult.PASS;
+	}
+
+	public static void onLevelLoad(ServerLevel serverLevel) {
+		Util.readProtectedList(serverLevel);
+	}
+}
---
+++ Common/src/main/java/com/natamus/starterstructure/events/StructureProtectionEvents.java
@@ -0,0 +1,107 @@
+package com.natamus.starterstructure.events;
+
+import com.natamus.collective.functions.ExplosionFunctions;
+import com.natamus.starterstructure.config.ConfigHandler;
+import com.natamus.starterstructure.util.Reference;
+import com.natamus.starterstructure.util.Util;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.decoration.painting.Painting;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class StructureProtectionEvents {
+	public static boolean onBlockBreak(Level level, Player player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
+		if (level.isClientSide()) {
+			return true;
+		}
+
+		if (ConfigHandler.playersInCreativeModeIgnoreProtection) {
+			if (player.isCreative()) {
+				return true;
+			}
+		}
+
+		if (Util.protectedMap.containsKey(level.dimension())) {
+			return !Util.protectedMap.get(level.dimension()).contains(pos);
+		}
+		return true;
+	}
+
+	public static boolean onBlockPlace(Level level, BlockPos blockPos, BlockState blockState, LivingEntity livingEntity, ItemStack itemStack) {
+		if (ConfigHandler.playersInCreativeModeIgnoreProtection) {
+			if (livingEntity instanceof Player) {
+				if (((Player) livingEntity).isCreative()) {
+					return true;
+				}
+			}
+		}
+
+		if (Util.protectedMap.containsKey(level.dimension())) {
+			return !Util.protectedMap.get(level.dimension()).contains(blockPos);
+		}
+		return true;
+	}
+
+	public static boolean onPistonMove(Level level, BlockPos blockPos, Direction direction, boolean isExtending) {
+		BlockPos faceOffsetPos = blockPos.relative(direction);
+		BlockPos nextPos = faceOffsetPos.relative(direction);
+
+		if (Util.protectedMap.containsKey(level.dimension())) {
+			return !Util.protectedMap.get(level.dimension()).contains(faceOffsetPos) && !Util.protectedMap.get(level.dimension()).contains(nextPos);
+		}
+		return true;
+	}
+
+	public static void onTNTExplode(Level level, Entity sourceEntity, Explosion explosion) {
+		if (level.isClientSide()) {
+			return;
+		}
+
+		if (explosion == null) {
+			return;
+		}
+
+		boolean cancel = false;
+		if (Util.protectedMap.containsKey(level.dimension())) {
+			for (BlockPos affectedPos : ExplosionFunctions.getAffectedBlockPositions(explosion)) {
+				if (Util.protectedMap.get(level.dimension()).contains(affectedPos)) {
+					cancel = true;
+					break;
+				}
+			}
+		}
+
+		if (cancel) {
+			ExplosionFunctions.clearExplosion(explosion);
+		}
+	}
+
+	public static boolean onEntityAttack(Player player, Level level, Entity targetEntity) {
+		if (ConfigHandler.playersInCreativeModeIgnoreEntityProtection) {
+			if (player.isCreative()) {
+				return true;
+			}
+		}
+
+		if (ConfigHandler.protectSpawnedEntities) {
+			if (targetEntity.entityTags().contains(Reference.MOD_ID + ".protected")) {
+				return false;
+			}
+		}
+
+		if (targetEntity instanceof Painting || targetEntity instanceof ItemFrame) {
+			if (Util.protectedMap.containsKey(level.dimension())) {
+				return !Util.protectedMap.get(level.dimension()).contains(targetEntity.blockPosition());
+			}
+		}
+		return true;
+	}
+}
