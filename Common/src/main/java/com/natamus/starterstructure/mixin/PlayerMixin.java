96c54f312357a40be53c685a2ceaa6f45df88e7c
Rick South <rick@natamus.com>
2026-06-21 12:06:22 +0200

    (v4.7) (26.1 / 26.2) Moved the ServerPlayer attack mixin to Player.

--- Common/src/main/java/com/natamus/starterstructure/mixin/PlayerMixin.java
+++ Common/src/main/java/com/natamus/starterstructure/mixin/PlayerMixin.java
@@ -3,19 +3,20 @@ package com.natamus.starterstructure.mixin;
 import com.natamus.starterstructure.events.StructureProtectionEvents;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ServerPlayer.class, priority = 1001)
-public class ServerPlayerMixin {
+@Mixin(value = Player.class, priority = 1001)
+public class PlayerMixin {
 	@Inject(method = "attack", at = @At("HEAD"), cancellable = true)
 	public void onPlayerInteractEntity(Entity targetEntity, CallbackInfo ci) {
-		ServerPlayer serverPlayer = (ServerPlayer)(Object)this;
-
-		if (!StructureProtectionEvents.onEntityAttack(serverPlayer, serverPlayer.level(), targetEntity)) {
-			ci.cancel();
+		if (((Player)(Object)this) instanceof ServerPlayer serverPlayer) {
+			if (!StructureProtectionEvents.onEntityAttack(serverPlayer, serverPlayer.level(), targetEntity)) {
+				ci.cancel();
+			}
 		}
 	}
 }
