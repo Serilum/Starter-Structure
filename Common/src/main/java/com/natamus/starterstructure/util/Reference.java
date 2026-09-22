6e026f06d6d5bf812fd71c9d26bab5a611c37d4e
Rick South <rick@natamus.com>
2026-09-22 16:02:46 +0200

    Updated Starter Structure to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/starterstructure/util/Reference.java
+++ Common/src/main/java/com/natamus/starterstructure/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "starterstructure";
 	public static final String NAME = "Starter Structure";
 	public static final String VERSION = "4.7";
-	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
+	public static final String ACCEPTED_VERSIONS = "[26.3.0]";
 }
